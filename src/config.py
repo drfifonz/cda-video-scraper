ef33f4c0821667fde2fc195cc757448d92d09dba
drfifonz <fp.patyk@gmail.com>
2022-10-25 17:15:07 +0200

    removed hardcoding, created full funcionality of scraper class

--- src/config.py
+++ src/config.py
@@ -1,6 +1,30 @@
-PATH_TO_FIREFOX = "resources/geckodriver-v0.31.0-linux64/geckodriver"
+PAGE_URL = "https://www.cda.pl/"
+PAGE_SEARCH_URL = "https://www.cda.pl/video/show/"
+PAGE_SEARCH_P = "p"
+PAGE_SEARCH_SORT_TYPE = "?s=best"
+
+
+FIREFOX_DRIVER_PATH = "resources/geckodriver-v0.31.0-linux64/geckodriver"
+FIREFOX_TO_DEV_NULL = "/dev/null"
 
 HEADERS_PATH = "data/headers.json"
 HEADERS_SECTION = "headers"
 
-PAGE_URL = "https://www.cda.pl/"
+# dict keys
+TITLES_DICT_KEY = "titles"
+DURATION_DICT_KEY = "duration"
+LINKS_DICT_KEY = "links"
+
+# html elements' unique tags, used to find certain data
+
+# static scraping with BeautifulSoup
+HREF_TAG = "href"
+A_TAG = "a"
+
+VIDEO_CLASS_TAG = "video-clip"
+LINK_CLASS_TAG = "video-clip-link autoHide"
+
+# dynamic scraping with Selenium
+TITLE_SEARCH_TAG = "h1"
+LINK_SEARCH_TAG = "video"
+LINK_ATTRIBUTE = "src"
--- src/scraper.py
+++ src/scraper.py
@@ -3,7 +3,8 @@ import os
 import re
 import time
 from random import choice
-from urllib import response
+
+# from urllib import response
 
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
@@ -17,9 +18,9 @@ import config as cfg
 
 class Scraper:
     def __init__(self) -> None:
-        self.headers = self._load_headers()
+        self.driver_service = Service(executable_path=cfg.FIREFOX_DRIVER_PATH, log_path=cfg.FIREFOX_TO_DEV_NULL)
 
-        self.service = Service(executable_path="resources/geckodriver-v0.31.0-linux64/geckodriver")
+        self.headers = self._load_headers()
 
         self.temp_header = self._get_random_header()
 
@@ -30,46 +31,75 @@ class Scraper:
     def _get_random_header(self) -> dict:
         return choice(self.headers)
 
-    def get_data_from_one_page(self, url: str) -> dict:
+    def _get_data_from_one_page(self, url: str) -> dict:
+        """
+        Gets videos data specified by search quote from single page
+
+        Returns dictionary of videos' :title, duration, link to video
+        """
         videos_dict = {}
 
         page = requests.get(url, self._get_random_header())
         doc = BeautifulSoup(page.text, "html.parser")
 
         if page.status_code != 200:
-            print(f" breaks scrapping with code: {page.status_code}")
+            print(f" Breaks scrapping with code: {page.status_code}")
             return None
 
-        videos = doc.find_all(class_="video-clip")
-        raw_links = [video.find_all(class_="video-clip-link autoHide")[0]["href"] for video in videos]
+        videos = doc.find_all(class_=cfg.VIDEO_CLASS_TAG)
+
+        raw_links = [video.find_all(class_=cfg.LINK_CLASS_TAG)[0][cfg.HREF_TAG] for video in videos]
         links = [cfg.PAGE_URL + link[1:] for link in raw_links]
-        raw_times = [video.find_all("a")[0].text for video in videos]
-        times = [time_text.strip().split()[-1] for time_text in raw_times]
-        titles = [video.find_all("a")[1].text for video in videos]
 
-        videos_dict["titles"] = titles
-        videos_dict["times"] = times
-        videos_dict["links"] = links
+        raw_duration = [video.find_all(cfg.A_TAG)[0].text for video in videos]
+        duration = [time_text.strip().split()[-1] for time_text in raw_duration]
 
-        # time.sleep(1)
+        titles = [video.find_all(cfg.A_TAG)[1].text for video in videos]
+
+        videos_dict[cfg.TITLES_DICT_KEY] = titles
+        videos_dict[cfg.DURATION_DICT_KEY] = duration
+        videos_dict[cfg.LINKS_DICT_KEY] = links
+
         return videos_dict
 
-    def get_data_from_pages(self, search_phrase: str, pages_num: int = 1) -> dict:
-        # pages_num = 1
-        videos_dict = {"titles": [], "times": [], "links": []}
-        for page_num in range(pages_num):
-            url = f"https://www.cda.pl/video/show/{search_phrase}/p{page_num+1}?s=best"
+    def get_data_from_pages(self, search_quote: str, pages_num: int = 1) -> dict:
+        """
+        gets videos data specified by search quote from certain pages number
 
-            one_page_videos_dict = self.get_data_from_one_page(url)
-            # print(page_num)
-            # print(url, "\n")
-            videos_dict["titles"] += one_page_videos_dict["titles"]
-            videos_dict["times"] += one_page_videos_dict["times"]
-            videos_dict["links"] += one_page_videos_dict["links"]
-            # print(dictionary["titles"])
+        Returns dictionary of videos' :title, duration, link to video
+        """
+        # pages_num = 1
+
+        videos_dict = {cfg.TITLES_DICT_KEY: [], cfg.DURATION_DICT_KEY: [], cfg.LINKS_DICT_KEY: []}
+        for page_num in range(pages_num):
+
+            url = os.path.join(
+                cfg.PAGE_SEARCH_URL, search_quote, cfg.PAGE_SEARCH_P + str(page_num + 1) + cfg.PAGE_SEARCH_SORT_TYPE
+            )
+
+            one_page_videos_dict = self._get_data_from_one_page(url)
+
+            videos_dict[cfg.TITLES_DICT_KEY] += one_page_videos_dict[cfg.TITLES_DICT_KEY]
+            videos_dict[cfg.DURATION_DICT_KEY] += one_page_videos_dict[cfg.DURATION_DICT_KEY]
+            videos_dict[cfg.LINKS_DICT_KEY] += one_page_videos_dict[cfg.LINKS_DICT_KEY]
+
             print(f"Data from page {page_num+1} collected.")
             time.sleep(1.5)
         return videos_dict
+
+    def get_direct_video_url(self, url: str) -> list[str, str]:
+        """
+        Gets download link from video url.
+        Return list of title & direct link to mp4 file.
+        """
+
+        with webdriver.Firefox(service=self.driver_service) as driver:
+            driver.minimize_window()
+            driver.get(url)
+
+            title = driver.find_element(By.TAG_NAME, cfg.TITLE_SEARCH_TAG).text
+            link = driver.find_element(By.TAG_NAME, cfg.LINK_SEARCH_TAG).get_attribute(cfg.LINK_ATTRIBUTE)
+            return [title, link]
 
     def download_video(self, video_url: str, video_name: str, path: str) -> None:
         if not os.path.exists(path):
@@ -83,12 +113,3 @@ class Scraper:
                 file.write(response.content)
         except:
             print(f"{video_name} already exists.")
-
-    def get_direct_video_url(self, url: str) -> list[str, str]:
-        with webdriver.Firefox(service=self.service) as driver:
-            driver.minimize_window()
-            driver.get(url)
-
-            title = driver.find_element(By.TAG_NAME, "h1").text
-            link = driver.find_element(By.TAG_NAME, "video").get_attribute("src")
-            return [title, link]
